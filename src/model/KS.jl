bfe1b101c112791c57d8821a62107eca0c321809
smallpondtom <tkoike45@gmail.com>
2023-08-23 15:25:51 -0400

    work on KSE

--- src/model/KS.jl
+++ src/model/KS.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra
 using SparseArrays
+using FFTW
 
 
 mutable struct KS
@@ -51,14 +52,13 @@ end
 function model_FD(model::KS, μ::Float64)
     N = model.Xdim
     Δx = model.Δx
-    Δt = model.Δt
 
     # Create A matrix
     ζ = 2/Δx^2 - 6*μ/Δx^4
     η = 4*μ/Δx^4 - 1/Δx^2
     ϵ = -μ/Δx^4
 
-    A = diagm(
+    A = spdiagm(
         0 => ζ * ones(N), 
         1 => η * ones(N - 1), -1 => η * ones(N - 1),
         2 => ϵ * ones(N - 2), -2 => ϵ * ones(N - 2)
@@ -86,4 +86,113 @@ function model_FD(model::KS, μ::Float64)
     end
 
     return A, F
+end
+
+
+function model_FFT(model::KS, μ::Float64)
+    N = model.Xdim
+    L = model.Omega[2]
+
+    # Create A matrix
+    A = spdiagm(
+        0 => [(2 * π * k / L)^2 - μ*(2 * π * k / L)^4 for k in -N/2:1.0:(N/2-1)]
+    )
+    
+    idx = 1  # index
+    F = spzeros(N, Int(N * (N + 1) / 2))
+    for k in -N/2:1.0:(N/2-1)
+        β = -π * 1.0im * k / L
+        F[idx, :] .= β
+        idx += 1
+    end       
+end
+
+
+function model_FT()
+end
+
+
+"""
+Half-vectorization operation
+
+# Arguments
+- `A`: matrix to half-vectorize
+
+# Return
+- `v`: half-vectorized form
+"""
+function vech(A::AbstractMatrix{T}) where {T}
+    m = LinearAlgebra.checksquare(A)
+    v = Vector{T}(undef, (m * (m + 1)) >> 1)
+    k = 0
+    for j = 1:m, i = j:m
+        @inbounds v[k+=1] = A[i, j]
+    end
+    return v
+end
+
+
+"""
+    Integrator using Crank-Nicholson Adams-Bashforth method
+
+    # Arguments
+    - `A`: A matrix
+    - `F`: F matrix
+    - `tdata`: temporal points
+    - `IC`: initial condition
+
+    # Return
+    - `state`: state matrix
+"""
+function integrator(A, F, tdata, IC)
+    Xdim = length(IC)
+    Tdim = length(tdata)
+    u = zeros(Xdim, Tdim)
+    u[:, 1] = IC
+    u2_lm1 = Vector{Float64}()  # u2 at j-2 placeholder
+
+    for j in 2:Tdim
+        Δt = tdata[j] - tdata[j-1]
+        u2 = vech(u[:, j-1] * u[:, j-1]')
+        if j == 2
+            u[:, j] = (1.0I(Xdim) - Δt/2 * A) \ ((1.0I(Xdim) + Δt/2 * A) * u[:, j-1] + F * u2 * Δt)
+        else
+            u[:, j] = (1.0I(Xdim) - Δt/2 * A) \ ((1.0I(Xdim) + Δt/2 * A) * u[:, j-1] + F * u2 * 3*Δt/2 - F * u2_lm1 * Δt/2)
+        end
+        u2_lm1 = u2
+    end
+    return u
+end
+
+
+
+function integrator_fourier(A, F, tdata, IC)
+    Xdim = length(IC)
+    Tdim = length(tdata)
+
+    # Plan the fourier transform and inverse fourier transform
+    prfft = plan_rfft(IC; flags=FFTW.ESTIMATE, timelimit=Inf)
+    pirfft = plan_irfft(IC, Xdim; flags=FFTW.ESTIMATE, timelimit=Inf)
+
+    u = zeros(Xdim, Tdim)  # state in the physical space
+    u[:, 1] = IC
+    uhat = zeros(Xdim, Tdim)  # state in the Fourier space
+    uhat[:, 1] = fftshift(prfft * u[:, 1]) / Xdim
+    uhat2_lm1 = Vector{Float64}()  # u2 at j-2 placeholder
+
+    for j in 2:Tdim
+        Δt = tdata[j] - tdata[j-1]
+        uhat2 = vech(uh[:, j-1] * uh[:, j-1]')
+
+        if j == 2
+            uhat[:, j] = (1.0I(Xdim) - Δt/2 * A) \ ((1.0I(Xdim) + Δt/2 * A) * uhat[:, j-1] + F * uhat2 * Δt)
+        else
+            uhat[:, j] = (1.0I(Xdim) - Δt/2 * A) \ ((1.0I(Xdim) + Δt/2 * A) * uhat[:, j-1] + F * uhat2 * 3*Δt/2 - F * uhat2_lm1 * Δt/2)
+        end
+        uhat2_lm1 = uhat2
+
+        # Get the state in the physical space
+        u[:, j] = pirfft * (ifftshift(uhat[:, j]))
+    end
+    return u, uhat
 end
