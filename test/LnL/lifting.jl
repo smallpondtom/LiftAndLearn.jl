c92516c4e5059de7175b330130a572d94ef09154
smallpondtom <tkoike45@gmail.com>
2023-12-12 17:36:48 -0500

    New tests

--- test/LnL/lifting.jl
+++ test/LnL/lifting.jl
@@ -42,5 +42,5 @@ end
          0.0       -0.810414   0.585857]
    X = [1 2 3 4 5 6; 7 8 9 10 11 12; 13 14 15 16 17 18; 19 20 21 22 23 24]  
    W = LnL.liftedBasis(X, 2, 2, [1,2])
-   @test round.(W, digits=6) ≈ D
+   @test all(isapprox.(W, D, atol=1e-5))
 end
