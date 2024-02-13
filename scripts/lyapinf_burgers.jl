41c3b58a349247caa259e2a90e50ff2b13f1d44d
smallpondtom <tkoike45@gmail.com>
2024-02-13 17:41:05 -0500

    Remove X^TPX constraint.

--- scripts/lyapinf_burgers.jl
+++ scripts/lyapinf_burgers.jl
@@ -90,7 +90,7 @@ op_inf = LnL.inferOp(X, zeros(Tdim_ds,1), zeros(Tdim_ds,1), Vrmax, Vrmax' * R, o
 # end
 
 ## Intrusive LyapInf for POD model
-ds2= 5  # another downsampling for lyapinf
+ds2= 1  # another downsampling for lyapinf
 int_lyapinf_options = LFI.Int_LyapInf_options(
     extra_iter=3,
     optimizer="Ipopt",
@@ -115,23 +115,8 @@ nonint_lyapinf_options = LFI.NonInt_LyapInf_options(
 )
 P_star, Q, cost, ∇cost = LFI.NonInt_LyapInf(Vrmax' * X[:,1:ds2:end], Vrmax' * R[:,1:ds2:end], nonint_lyapinf_options)
 
-##
-# function skp_stability_rad(Ahat::AbstractArray{T}, Hhat::AbstractArray{T}, Q::AbstractArray{T}; 
-#         div_by_2::Bool=false) where T
 
-#     if div_by_2
-#         P = lyapc(Ahat', 0.5*Q)
-#     else
-#         P = lyapc(Ahat', Q)
-#     end
-#     L = cholesky(Q).L
-#     σmin = minimum(svd(L).S)
-#     ρhat = σmin / sqrt(norm(P,2)) / norm(Hhat,2) / 2
-#     return ρhat
-# end
-
-
-## Sample the correct level surface
+## Sample the correct level surface (for r = 10)
 ## POD
 V = (x) -> x' * P_int * x
 Vdot = (x) -> x' * P_int * op_int.A * x + x' * P_int * op_int.F * (x ⊘ x)
@@ -158,11 +143,11 @@ fig1
 ## OpInf
 V = (x) -> x' * P_inf * x
 Vdot = (x) -> x' * P_inf * op_inf.A * x + x' * P_inf * op_inf.F * (x ⊘ x)
-c_star2, c_all2, x_sample2 = LFI.doa_sampling(
+c_star2 = LFI.doa_sampling(
     V,
     Vdot,
     1e6, rmax, Tuple(burgers.Omega);
-    method="memory", history=true, uniform_state_space=true
+    method="memory", history=false, uniform_state_space=true
 )
 ρmin2 = sqrt(c_star2/maximum(eigvals(P_inf)))
 ρskp2 = LFI.skp_stability_rad(op_inf.A, op_inf.H, Q)
@@ -170,11 +155,77 @@ c_star2, c_all2, x_sample2 = LFI.doa_sampling(
 ## Non-intrusive
 V = (x) -> x' * P_star * x
 Vdot = (x) -> x' * P_star * op_int.A * x + x' * P_star * op_int.F * (x ⊘ x)
-c_star3, c_all3, x_sample3 = LFI.doa_sampling(
+c_star3 = LFI.doa_sampling(
     V,
     Vdot,
     1e6, rmax, Tuple(burgers.Omega);
-    method="memory", history=true, uniform_state_space=true
+    method="memory", history=false, uniform_state_space=true
 )
 ρmin3 = sqrt(c_star3/maximum(eigvals(P_star)))
 ρskp3 = LFI.skp_stability_rad(op_int.A, op_int.H, Q)
+
+## Plot the DoA for all reduced dimensions
+ρ_all = zeros(rmax,3)
+ρskp_all = zeros(rmax,2)
+fig2 = Figure(size=(900,600), fontsize=20)
+ax = Axis(fig2[1,1],
+    ylabel=L"DoA $$",
+    xlabel=L"reduced dimension, $r$",
+    xticks=rmin:rmax,
+    yscale=log10,
+)
+for (i,r) in enumerate(rmin:rmax)
+    # Intrusive POD
+    P = P_int[1:r, 1:r]
+    A = op_int.A[1:r, 1:r]
+    H = LnL.extractH(op_int.H, r)
+    F = LnL.extractF(op_int.F, r)
+    V = (x) -> x' * P * x
+    Vdot = (x) -> x' * P * A * x + x' * P * F * (x ⊘ x)
+    c_star = LFI.doa_sampling(
+        V,
+        Vdot,
+        1000000, r, (-10,10);
+        method="memory", history=false, uniform_state_space=true
+    )
+    ρ_all[i,1] = sqrt(c_star/maximum(eigvals(P)))
+    ρskp_all[i,1] = LFI.skp_stability_rad(A, H, Q[1:r,1:r])
+
+    # OpInf
+    P = P_inf[1:r, 1:r]
+    A = op_inf.A[1:r, 1:r]
+    H = LnL.extractH(op_inf.H, r)
+    F = LnL.extractF(op_inf.F, r)
+    V = (x) -> x' * P * x
+    Vdot = (x) -> x' * P * A * x + x' * P * F * (x ⊘ x)
+    c_star = LFI.doa_sampling(
+        V,
+        Vdot,
+        1000000, r, (-10,10);
+        method="memory", history=false, uniform_state_space=true
+    )
+    ρ_all[i,2] = sqrt(c_star/maximum(eigvals(P)))
+    ρskp_all[i,2] = LFI.skp_stability_rad(A, H, Q[1:r,1:r])
+
+    # Non-intrusive
+    P = P_star[1:r, 1:r]
+    A = op_int.A[1:r, 1:r]
+    H = LnL.extractH(op_int.H, r)
+    F = LnL.extractF(op_int.F, r)
+    V = (x) -> x' * P * x
+    Vdot = (x) -> x' * P * A * x + x' * P * F * (x ⊘ x)
+    c_star = LFI.doa_sampling(
+        V,
+        Vdot,
+        1000000, r, (-10,10);
+        method="memory", history=false, uniform_state_space=true
+    )
+    ρ_all[i,3] = sqrt(c_star/maximum(eigvals(P)))
+end
+scatterlines!(ax, rmin:rmax, ρ_all[:,1], label="POD")
+scatterlines!(ax, rmin:rmax, ρ_all[:,2], label="OpInf")
+scatterlines!(ax, rmin:rmax, ρ_all[:,3], label="Non-intrusive")
+scatterlines!(ax, rmin:rmax, ρskp_all[:,1], label="POD (SKP)", linestyle=:dash)
+scatterlines!(ax, rmin:rmax, ρskp_all[:,2], label="OpInf (SKP)", linestyle=:dash)
+fig2[1,2] = Legend(fig2, ax)
+fig2
