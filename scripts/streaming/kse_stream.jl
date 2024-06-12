2f3e01818986fd724b330e6db2a68cbc006293b0
smallpondtom <tkoike45@gmail.com>
2024-06-12 11:12:53 -0400

    got kse streaming example done

--- scripts/streaming/kse_stream.jl
+++ scripts/streaming/kse_stream.jl
@@ -6,6 +6,7 @@
 ## Packages
 #############
 using CairoMakie
+using DelimitedFiles
 using LinearAlgebra
 using ProgressMeter
 
@@ -21,6 +22,8 @@ const LnL = LiftAndLearn
 ## Global Settings
 ###################
 CONST_STREAM = true
+SAVEDATA = false
+LOADDATA = SAVEDATA ? false : true
 SAVEFIG = true
 
 
@@ -51,12 +54,13 @@ options = LnL.LSOpInfOption(
     ),
     data=LnL.DataStructure(
         Δt=1e-3,
+        DS=100,
     ),
     optim=LnL.OptimizationSetting(
         verbose=true,
     ),
 )
-
+DS = 100  # downsample rate of data
 # Parameterized function for the initial condition
 L = kse.Omega[2] - kse.Omega[1]  # length of the domain
 u0 = (a,b) -> a * cos.((2*π*kse.x)/L) .+ b * cos.((4*π*kse.x)/L)  # initial condition
@@ -68,32 +72,92 @@ u0 = (a,b) -> a * cos.((2*π*kse.x)/L) .+ b * cos.((4*π*kse.x)/L)  # initial co
 A, F = kse.model_FD(kse, kse.μs[1])
 C = ones(1, kse.Xdim) / kse.Xdim
 op_kse = LnL.operators(A=A, C=C, F=F)
-a = 0.8; b = 1.2
 
-# Training data (which is also Reference solution)
-X = kse.integrate_FD(A, F, kse.t, u0(a,b))
-Y = C * X
+# Initial condition parameters
+a = [0.8, 1.0, 1.2]
+b = [0.2, 0.4, 0.6]
+num_ic_params = Int(length(a) * length(b))
 
-# compute the POD basis from the training data
-rmax = 150
-Vrmax = svd(X).U[:, 1:rmax]
-Σr = svd(X).S[1:rmax]
 
-# Save the reference solution
-Xref = copy(X)
-Yref = copy(Y)
+if LOADDATA
+    X = readdlm("scripts/streaming/data/kse/X.csv", ',')
+    Y = readdlm("scripts/streaming/data/kse/Y.csv", ',')
+    Xdot = readdlm("scripts/streaming/data/kse/Xdot.csv", ',')
+    Xref = readdlm("scripts/streaming/data/kse/Xref.csv", ',')
+    Yref = readdlm("scripts/streaming/data/kse/Yref.csv", ',')
+    Vrmax = readdlm("scripts/streaming/data/kse/Vrmax.csv", ',')
+    Σr = readdlm("scripts/streaming/data/kse/Σr.csv", ',')
+else
+    @info "Generate reference solution data..."
+    # Reference solution
+    a_ref = sum(a) / length(a)
+    b_ref = sum(b) / length(b)
+    Xref = kse.integrate_FD(A, F, kse.t, u0(a_ref,b_ref))
+    Yref = C * Xref
+    @info "Done"
+
+    @info "Generate training data..."
+    # Store the training data 
+    Xall = Vector{Matrix{Float64}}(undef, num_ic_params)
+    Yall = Vector{Matrix{Float64}}(undef, num_ic_params)
+    Xdotall = Vector{Matrix{Float64}}(undef, num_ic_params)
+
+    # Generate the data for all combinations of the initial condition parameters
+    ab_combos = collect(Iterators.product(a, b))
+    prog = Progress(length(ab_combos))
+    Threads.@threads for (i, ic) in collect(enumerate(ab_combos))
+        ai, bi = ic
+        states = kse.integrate_FD(A, F, kse.t, u0(ai,bi))
+        tmp = states[:, 2:end]
+        Yall[i] = C * tmp[:, 1:DS:end]  # downsample data
+        Xall[i] = tmp[:, 1:DS:end]  # downsample data
+        tmp = (states[:, 2:end] - states[:, 1:end-1]) / kse.Δt
+        Xdotall[i] = tmp[:, 1:DS:end]  # downsample data
+        next!(prog)
+    end
+    # Combine all initial condition data to form on big training data matrix
+    X = reduce(hcat, Xall) 
+    Y = reduce(hcat, Yall)
+    Xdot = reduce(hcat, Xdotall)
+    @info "Done"
+
+    # compute the POD basis from the training data
+    rmax = 150
+    Vrmax = svd(X).U[:, 1:rmax]
+    Σr = svd(X).S[1:rmax]
+
+    # Discard unnecessary variables
+    Xall = nothing
+    Yall = nothing
+    Xdotall = nothing
+    GC.gc()
+end
+
+
+#############
+## Save data
+#############
+if SAVEDATA
+    writedlm("scripts/streaming/data/kse/X.csv", X, ",")
+    writedlm("scripts/streaming/data/kse/Y.csv", Y, ",")
+    writedlm("scripts/streaming/data/kse/Xdot.csv", Xdot, ",")
+    writedlm("scripts/streaming/data/kse/Xref.csv", Xref, ",")
+    writedlm("scripts/streaming/data/kse/Yref.csv", Yref, ",")
+    writedlm("scripts/streaming/data/kse/Vrmax.csv", Vrmax, ",")
+    writedlm("scripts/streaming/data/kse/Σr.csv", Σr, ",")
+end
 
 
 ######################
 ## Plot Data to Check
 ######################
 with_theme(theme_latexfonts()) do
-    DS = 100
+    tmp = 100
     fig0 = Figure(fontsize=20, size=(1300,500), backgroundcolor="#FFFFFF")
     ax1 = Axis3(fig0[1, 1], xlabel="x", ylabel="t", zlabel="u(x,t)")
-    surface!(ax1, kse.x, kse.t[1:DS:end], X[:,1:DS:end])
+    surface!(ax1, kse.x, kse.t[1:tmp:end], Xref[:,1:tmp:end])
     ax2 = Axis(fig0[1, 2], xlabel="x", ylabel="t")
-    hm = heatmap!(ax2, kse.x, kse.t[1:DS:end], X[:,1:DS:end])
+    hm = heatmap!(ax2, kse.x, kse.t[1:tmp:end], Xref[:,1:tmp:end])
     Colorbar(fig0[1, 3], hm)
     display(fig0)
 end
@@ -132,9 +196,6 @@ op_int = LnL.pod(op_kse, Vr, options)
 ## OpInf model
 ###############
 # Choose the correct indices corresponding to the derivative data
-Xdot = (X[:, 2:end] - X[:, 1:end-1]) / kse.Δt
-X = X[:, 2:end]
-Y = Y[:, 2:end]
 op_inf = LnL.opinf(X, Vr, options; Y=Y, Xdot=Xdot)
 
 
@@ -145,8 +206,7 @@ options.with_reg = true
 options.λ = LnL.TikhonovParameter(
     lin = 1e-7,
     quad = 1e-7,
-    ctrl = 1e-7,
-    output = 1e-6
+    output = 1e-5
 )
 op_inf_reg = LnL.opinf(X, Vr, options; Y=Y, Xdot=Xdot)
 
@@ -172,9 +232,9 @@ num_of_streams = length(Xhat_stream)
 
 # Initialize the stream
 γs = 1e-7
-γo = 1e-6
-algo=:RLS
-stream = LnL.StreamingOpInf(options, rmax, 0, size(Y,1); variable_regularize=false, γs_k=γs, γo_k=γo, algorithm=algo)
+γo = 1e-5
+algo=:iQRRLS
+stream = LnL.StreamingOpInf(options, maximum(r_select), 0, size(Y,1); variable_regularize=false, γs_k=γs, γo_k=γo, algorithm=algo)
 
 # Stream all at once
 stream.stream!(stream, Xhat_stream, R_stream)
@@ -182,6 +242,12 @@ stream.stream_output!(stream, Xhat_stream, Y_stream)
 
 # Unpack solution operators
 op_stream = stream.unpack_operators(stream)
+
+# Remove unnecessary variables
+Xhat_stream = nothing
+Y_stream = nothing
+R_stream = nothing
+GC.gc()
 
 
 ###############################
@@ -192,11 +258,40 @@ op_dict = Dict(
     "POD" => op_int,
     "OpInf" => op_inf,
     "TR-OpInf" => op_inf_reg,
-    # "iQRRLS-Streaming-OpInf" => op_stream
+    "TR-Streaming-OpInf" => op_stream
 )
-rse, roe = analysis_1(op_dict, kse, Vr, Xref, [], Yref, [:A, :F], kse.integrate_FD)
+kse.IC = u0(a_ref, b_ref)  # make sure to modify the initial condition
+rse, roe = analysis_1(op_dict, kse, Vr, Xref, [], Yref, [:A, :F], kse.integrate_FD; r_select=r_select)
 
 ## Plot
-# fig1 = plot_rse(rse, roe, rmax, ace_light; provided_keys=["POD", "OpInf", "TR-OpInf", "iQRRLS-Streaming-OpInf"])
-fig1 = plot_rse(rse, roe, rmax, ace_light; provided_keys=["POD", "OpInf", "TR-OpInf"])
+fig1 = plot_rse(rse, roe, rmax, ace_light; provided_keys=["POD", "OpInf", "TR-OpInf", "TR-Streaming-OpInf"])
+# fig1 = plot_rse(rse, roe, maximum(r_select), ace_light; provided_keys=["POD", "OpInf"])
 display(fig1)
+
+
+######################
+## Qualitative Result
+######################
+Xtmp = kse.integrate_FD(op_stream.A, op_stream.F, kse.t, Vr' * u0(a_ref,b_ref))
+
+with_theme(theme_latexfonts()) do
+    tmp = 100
+    fig0 = Figure(fontsize=20, size=(1300,500))
+    ax1 = Axis3(fig0[1, 1], xlabel="x", ylabel="t", zlabel="u(x,t)")
+    surface!(ax1, kse.x, kse.t[1:tmp:end], (Vr * Xtmp)[:,1:tmp:end])
+    ax2 = Axis(fig0[1, 2], xlabel="x", ylabel="t")
+    hm = heatmap!(ax2, kse.x, kse.t[1:tmp:end], (Vr * Xtmp)[:,1:tmp:end])
+    Colorbar(fig0[1, 3], hm)
+    display(fig0)
+end
+
+with_theme(theme_latexfonts()) do
+    tmp = 100
+    fig0 = Figure(fontsize=20, size=(1300,500))
+    ax1 = Axis3(fig0[1, 1], xlabel="x", ylabel="t", zlabel="u(x,t)")
+    surface!(ax1, kse.x, kse.t[1:tmp:end], (Vr * Xtmp - Xref)[:,1:tmp:end], colormap=:roma)
+    ax2 = Axis(fig0[1, 2], xlabel="x", ylabel="t")
+    hm = heatmap!(ax2, kse.x, kse.t[1:tmp:end], (Vr * Xtmp - Xref)[:,1:tmp:end], colormap=:roma)
+    Colorbar(fig0[1, 3], hm)
+    display(fig0)
+end
