eef625e6a493bbec762d74e23da6cb35568f9dbb
smallpondtom <tkoike45@gmail.com>
2024-08-28 08:48:33 -0400

    make sure to get input matrix U dim right

--- src/utilities/integrator.jl
+++ src/utilities/integrator.jl
@@ -20,9 +20,15 @@ function forwardEuler(A::Array, B::Array, U::Array, tdata::VecOrMat, IC::VecOrMa
     Tdim = length(tdata)
     states = zeros(Xdim, Tdim)
     states[:, 1] = IC
+
+    # If row dim corresponds to # of time steps, transpose the input data
+    if size(U, 1) == Tdim
+        U = U'
+    end
+
     for j in 2:Tdim
         Δt = tdata[j] - tdata[j-1]
-        states[:, j] = (I(Xdim) + Δt * A) * states[:, j-1] + B * U[j-1] * Δt
+        states[:, j] = (I(Xdim) + Δt * A) * states[:, j-1] + B * U[:,j-1] * Δt
     end
     return states
 end
@@ -74,9 +80,15 @@ function forwardEuler(f::Function, U::Array, tdata::VecOrMat, IC::VecOrMat)::Mat
     Tdim = length(tdata)
     states = zeros(Xdim, Tdim)
     states[:, 1] = IC
+
+    # If row dim corresponds to # of time steps, transpose the input data
+    if size(U, 1) == Tdim
+        U = U'
+    end
+
     @inbounds @views for j in 2:Tdim
         Δt = tdata[j] - tdata[j-1]
-        states[:, j] = states[:, j-1] + Δt * f(states[:, j-1], U[j])
+        states[:, j] = states[:, j-1] + Δt * f(states[:, j-1], U[:,j])
     end
     return states
 end
@@ -102,9 +114,15 @@ function backwardEuler(A, B, U, tdata, IC)
     Tdim = length(tdata)
     state = zeros(Xdim, Tdim)
     state[:, 1] = IC
+
+    # If row dim corresponds to # of time steps, transpose the input data
+    if size(U, 1) == Tdim
+        U = U'
+    end
+
     @inbounds @views for j in 2:Tdim
         Δt = tdata[j] - tdata[j-1]
-        state[:, j] = (I(Xdim) - Δt * A) \ (state[:, j-1] + B * U[j-1] * Δt)
+        state[:, j] = (I(Xdim) - Δt * A) \ (state[:, j-1] + B * U[:,j-1] * Δt)
     end
     return state
 end
@@ -131,9 +149,15 @@ function crankNicolson(A, B, U, tdata, IC)
     Tdim = length(tdata)
     states = zeros(Xdim, Tdim)
     states[:, 1] = IC
+
+    # If row dim corresponds to # of time steps, transpose the input data
+    if size(U, 1) == Tdim
+        U = U'
+    end
+
     @inbounds @views for j in 2:Tdim
         Δt = tdata[j] - tdata[j-1]
-        states[:, j] = (I(Xdim) - 0.5 * Δt * A) \ ((I(Xdim) + 0.5 * Δt * A) * states[:, j-1] + B * U[j-1] * Δt)
+        states[:, j] = (I(Xdim) - 0.5 * Δt * A) \ ((I(Xdim) + 0.5 * Δt * A) * states[:, j-1] + B * U[:,j-1] * Δt)
     end
     return states
 end
@@ -161,11 +185,16 @@ function semiImplicitEuler(A, B, F, U, tdata, IC)
     state = zeros(Xdim, Tdim)
     state[:, 1] = IC
 
+    # If row dim corresponds to # of time steps, transpose the input data
+    if size(U, 1) == Tdim
+        U = U'
+    end
+
     @inbounds @views for j in 2:Tdim
         Δt = tdata[j] - tdata[j-1]
         # state2 = vech(state[:, j-1] * state[:, j-1]')
         state2 = state[:, j-1] ⊘ state[:, j-1]
-        state[:, j] = (I(Xdim) - Δt * A) \ (state[:, j-1] + F * state2 * Δt + B * U[j-1] * Δt)
+        state[:, j] = (I(Xdim) - Δt * A) \ (state[:, j-1] + F * state2 * Δt + B * U[:,j-1] * Δt)
     end
     return state
 end
@@ -192,19 +221,24 @@ function semiImplicitEuler(A, B, F_or_H, U, tdata, IC, options)
     Tdim = length(tdata)
     state = zeros(Xdim, Tdim)
     state[:, 1] = IC
+
+    # If row dim corresponds to # of time steps, transpose the input data
+    if size(U, 1) == Tdim
+        U = U'
+    end
     
     if options.which_quad_term == "F"
         @inbounds @views for j in 2:Tdim
             Δt = tdata[j] - tdata[j-1]
             # state2 = vech(state[:, j-1] * state[:, j-1]')
             state2 = state[:, j-1] ⊘ state[:, j-1]
-            state[:, j] = (I(Xdim) - Δt * A) \ (state[:, j-1] + F_or_H * state2 * Δt + B * U[j-1] * Δt)
+            state[:, j] = (I(Xdim) - Δt * A) \ (state[:, j-1] + F_or_H * state2 * Δt + B * U[:,j-1] * Δt)
         end
     elseif options.which_quad_term == "H"
         @inbounds @views for j in 2:Tdim
             Δt = tdata[j] - tdata[j-1]
             state2 = vec(state[:, j-1] * state[:, j-1]')
-            state[:, j] = (I(Xdim) - Δt * A) \ (state[:, j-1] + F_or_H * state2 * Δt + B * U[j-1] * Δt)
+            state[:, j] = (I(Xdim) - Δt * A) \ (state[:, j-1] + F_or_H * state2 * Δt + B * U[:,j-1] * Δt)
         end
     end
     return state
