704c067987d1b3de6136de019f6183263bc75e03
smallpondtom <tkoike45@gmail.com>
2025-05-27 11:40:30 -0400

    Modify the basis file for 3d chanmel

--- scripts/Two-Pass_Streaming-OpInf/3d_channel/01-basis.jl
+++ scripts/Two-Pass_Streaming-OpInf/3d_channel/01-basis.jl
@@ -12,6 +12,7 @@ using JLD2
 using IncrementalSVD
 using LinearAlgebra
 using ProgressMeter
+using SparseArrays
 import LiftAndLearn as LnL
 
 #==========================================#
@@ -33,10 +34,22 @@ datafile = joinpath(DATAPATH, fn)
 ## Load the training dataset
 #=============================#
 ds = ChannelDataSource(datafile, ["z", "y", "x", "fields", "times"])
+Nz, Ny, Nx, n_fields, n = ds.dims
 
 #=========================================================#
 ## Generate the POD basis using iSVD using all algorithms
 #=========================================================#
+# (Dry) Run it once with a dummy due to JUlia's JIT compilation
+Xdummy = rand(30, 200)
+baker = iSVD(x1=Xdummy[:,1], algo=:baker, max_rank=4) 
+full_increment!(baker, Xdummy, verbose=true, runtime=true)
+brand = iSVD(x1=Xdummy[:,1], algo=:brand1, reorth_method=:qr, max_rank=4)
+full_increment!(brand, Xdummy, verbose=true, tol=1e-10, runtime=true)
+sketchy = iSVD(algo=:sketchy; m=size(Xdummy,1), n=size(Xdummy,2), r=4, ReduxMap=:Sparse)
+full_increment!(sketchy, Xdummy, verbose=true, runtime=true, dump_all=true)
+svd(Xdummy)
+
+## Settings
 rmax = 100
 Xall = Array[]
 
@@ -45,48 +58,62 @@ time_baker = []
 time_brand = []
 time_sketchy = []
 
-## (Dry) Run it once with a dummy due to JUlia's JIT compilation
-Xdummy = rand(30, 200)
-baker = iSVD(x1=Xdummy, algo=:baker, max_rank=4) 
-full_increment!(baker, Xdummy, verbose=true, runtime=true)
-brand = iSVD(x1=Xdummy, algo=:brand1, reorth_method=:qr, max_rank=4)
-full_increment!(brand, Xdummy, verbose=true, tol=1e-10, runtime=true)
-sketchy = iSVD(algo=:sketchy; m=size(Xdummy,1), n=size(Xdummy,2), r=4, ReduxMap=:Sparse)
-full_increment!(sketchy, Xdummy, verbose=true, runtime=true, dump_all=true)
-svd(Xdummy)
-
-
 ## baker
-tmp = @elapsed baker = iSVD(x1=X[:,1], algo=:baker, max_rank=rmax)
+tmp = @elapsed baker = iSVD(x1=ds[1], algo=:baker, max_rank=rmax)
 push!(time_baker, tmp)
-tmp = full_increment!(baker, X[:,2:end], verbose=true, runtime=true)
-push!(time_baker, tmp)
+@showprogress for i in 2:n 
+    tmp = @elapsed increment!(baker, ds[i])
+    push!(time_baker, tmp)
+end
 
 ## brand
-tmp = @elapsed brand = iSVD(x1=X[:,1], algo=:brand1, reorth_method=:gramschmidt, max_rank=rmax)
+tmp = @elapsed brand = iSVD(x1=ds[1], algo=:brand1, reorth_method=:gramschmidt, max_rank=rmax)
 push!(time_brand, tmp)
-tmp = full_increment!(brand, X[:,2:end], verbose=true, tol=1e-10, runtime=true)
-push!(time_brand, tmp)
+@showprogress for i in 2:n 
+    tmp = @elapsed increment!(brand, ds[i], tol=1e-10)
+    push!(time_brand, tmp)
+end
 
 ## sketchy
-tmp = @elapsed sketchy = iSVD(algo=:sketchy; m=Nx*Ny, n=n, r=rmax, ReduxMap=:Sparse)
+tmp = @elapsed sketchy = iSVD(algo=:sketchy; m=Nx*Ny*Nz*n_fields, n=n, r=rmax, ReduxMap=:Sparse)
 push!(time_sketchy, tmp)
-tmp = full_increment!(sketchy, X, verbose=true, runtime=true, dump_all=true)
-push!(time_sketchy, tmp.runtime)
+X = spzeros(Nx*Ny*Nz*n_fields, n)
+@showprogress for i in 1:(n ÷ 10)
+    idx = 10*(i-1)+1:10*i
+    X[:,idx] .= ds[idx]
+    sketchy.X .+= sketchy.Ξ * X
+    sketchy.Y .+= X * sketchy.Ω'
+    sketchy.Z .+= (sketchy.Φ * X) * sketchy.Ψ'
+    push!(time_sketchy, tmp)
+    fill!(X, 0)
+    dropzeros!(X)
+end
+IncrementalSVD.terminate!(sketchy, false, false)
 
 #============================================#
 ## Compute the POD basis using the batch SVD 
 #============================================#
-time_batch = @elapsed F = svd(X)
+try
+    time_batch = @elapsed F = svd(ds[1:n])
+catch e
+    if isa(e, OutOfMemoryError)
+        @error "Out of memory error during SVD computation. Using randomized SVD."
+    end
+    try
+        time_batch = @elapsed F = rsvd(ds[1:n], rmax, p=10)
+    catch e2
+        @error "Out of memory for randomized SVD as well. We're out of luck for batch methods."
+    end
+end
 
 #=====================================================================#
 ## Save the POD basis and singular values from the iSVD and batch SVD
 #=====================================================================#
 bases = Dict(
     "baker" => (iVr=baker.Q[:,1:rmax], iΣr=baker.Σ[1:rmax]),
-    "brand" => (iVr=brand.Q[:,1:rmax], iΣr=brand.Σ[1:rmax]),
-    "sketchy" => (iVr=sketchy.Q[:,1:rmax], iΣr=sketchy.Σ[1:rmax]),
-    "batch" => (Vr=F.U[:,1:rmax], Σr=F.S[1:rmax]),
+    # "brand" => (iVr=brand.Q[:,1:rmax], iΣr=brand.Σ[1:rmax]),
+    # "sketchy" => (iVr=sketchy.Q[:,1:rmax], iΣr=sketchy.Σ[1:rmax]),
+    # "batch" => (Vr=F.U[:,1:rmax], Σr=F.S[1:rmax]),
 )
 save(joinpath(FILEPATH, "data/streaming/basis.jld2"), bases)
 
@@ -94,12 +121,13 @@ save(joinpath(FILEPATH, "data/streaming/basis.jld2"), bases)
 ## Save the runtime of the iSVD algorithms over all streams
 #============================================================#
 time_baker = reduce(vcat, time_baker)
-time_brand = reduce(vcat, time_brand)
+# time_brand = reduce(vcat, time_brand)
 # time_sketchy = reduce(vcat, time_sketchy)
 save(
     joinpath(FILEPATH, "data/streaming/basis_runtime.jld2"),
-    "baker", time_baker, "brand", time_brand,  
-    "batch", time_batch,  "sketchy", time_sketchy, 
+    "baker", time_baker, 
+    # "brand", time_brand,  
+    # "batch", time_batch,  "sketchy", time_sketchy, 
 )
 
 #================================#
@@ -111,11 +139,24 @@ proj_error = Dict(
     "sketchy" => zeros(rmax),
     "batch" => zeros(rmax),
 )
-for i in 1:rmax
-    proj_error["baker"][i] = norm(X - bases["baker"].iVr[:,1:i] * bases["baker"].iVr[:,1:i]' * X, 2) / norm(X, 2)
-    proj_error["brand"][i] = norm(X - bases["brand"].iVr[:,1:i] * bases["brand"].iVr[:,1:i]' * X, 2) / norm(X, 2)
-    proj_error["sketchy"][i] = norm(X - bases["sketchy"].iVr[:,1:i] * bases["sketchy"].iVr[:,1:i]' * X, 2) / norm(X, 2)
-    proj_error["batch"][i] = norm(X - bases["batch"].Vr[:,1:i] * bases["batch"].Vr[:,1:i]' * X, 2) / norm(X, 2)
+for i in [100]
+    mult = 1
+    tot_norm = 0.0
+    # @showprogress @inbounds @simd for j in 1:(n ÷ mult)
+    for j in 1:n
+        # X = ds[mult*(j-1)+1:mult*j]
+        X = ds[j]
+        proj_error["baker"][i] += norm(
+            X - bases["baker"].iVr[:,1:i] * bases["baker"].iVr[:,1:i]' * X, 2
+        )
+        tot_norm += norm(X, 2)
+        println("j = $j, i = $i, error = $(proj_error["baker"][i] / tot_norm)")
+    end
+    proj_error["baker"][i] /= tot_norm
+    @info "Projection error for baker at rank $i: $(proj_error["baker"][i])"
+    # proj_error["brand"][i] = norm(X - bases["brand"].iVr[:,1:i] * bases["brand"].iVr[:,1:i]' * X, 2) / norm(X, 2)
+    # proj_error["sketchy"][i] = norm(X - bases["sketchy"].iVr[:,1:i] * bases["sketchy"].iVr[:,1:i]' * X, 2) / norm(X, 2)
+    # proj_error["batch"][i] = norm(X - bases["batch"].Vr[:,1:i] * bases["batch"].Vr[:,1:i]' * X, 2) / norm(X, 2)
 end
 
 save(joinpath(FILEPATH, "data/projection_errors.jld2"), proj_error)            
