bb846628e5cee42ba8856addca165fb7ea3debe4
smallpondtom <tkoike45@gmail.com>
2025-07-23 10:28:33 -0400

    tried MHD64 data

---
+++ scripts/Two-Pass_Streaming-OpInf/mhd64/00-preprocess.jl
@@ -0,0 +1,216 @@
+"""
+Supernova 64^3 example: Preprocessing the data
+"""
+
+#=================#
+## Load packages ##
+#=================#
+using LinearAlgebra
+using FileIO
+using JLD2
+using Statistics
+
+#=============#
+## Load data ##
+#=============#
+# Get paths and data file name
+FILEPATH = occursin("scripts", pwd()) ? 
+           joinpath(pwd(), "Two-Pass_Streaming-OpInf/mhd64") : 
+           joinpath(pwd(), "scripts/Two-Pass_Streaming-OpInf/mhd64")
+DATAPATH = "../../../../DATA/THE_WELL/mhd64"
+train_files = readdir(DATAPATH, join=true)
+fn = train_files[1]
+
+# Include the data sourcing module for data access
+include(joinpath(FILEPATH, "datasource.jl"))
+
+# Load data source 
+ds = DataSource(fn)
+nx, ny, nz, n_fields, n_time, n_traj = ds.dims
+nxyz = nx * ny * nz
+
+# Flag for float64
+USE_FLOAT64 = true
+
+# Flag to center
+CENTER_DATA = true
+SCALE_DATA = true
+
+#====================#
+## Preprocess data  ##
+#====================#
+n = n_time * n_traj
+if USE_FLOAT64
+    @info "Using Float64 for data"
+    Xr_orig  = Float64.(reshape(ds["rho"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+    Xu_orig  = Float64.(reshape(ds["u"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+    Xv_orig  = Float64.(reshape(ds["v"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+    Xw_orig  = Float64.(reshape(ds["w"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+    Xbx_orig = Float64.(reshape(ds["Bx"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+    Xby_orig = Float64.(reshape(ds["By"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+    Xbz_orig = Float64.(reshape(ds["Bz"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+else
+    @info "Using Float32 for data"
+    Xr_orig  = Float32.(reshape(ds["rho"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+    Xu_orig  = Float32.(reshape(ds["u"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+    Xv_orig  = Float32.(reshape(ds["v"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+    Xw_orig  = Float32.(reshape(ds["w"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+    Xbx_orig = Float32.(reshape(ds["Bx"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+    Xby_orig = Float32.(reshape(ds["By"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+    Xbz_orig = Float32.(reshape(ds["Bz"][1:nxyz, 1:n_time, 1:n_traj], nxyz, n))
+end
+X_orig = vcat(Xr_orig, Xu_orig, Xv_orig, Xw_orig, Xbx_orig, Xby_orig, Xbz_orig)
+
+## Save unscaled/unshifted data
+original_file = joinpath(FILEPATH, "data/original_data.jld2")
+if !isfile(original_file)
+    @info "Saving original data to file"
+    save(original_file, 
+        "X", Dict(
+            "rho" => Xr_orig, "u" => Xu_orig, "v" => Xv_orig, "w" => Xw_orig,
+            "Bx" => Xbx_orig, "By" => Xby_orig, "Bz" => Xbz_orig,
+            "all" => X_orig
+        ),
+    )
+end
+
+## Copy original data
+Xr = copy(Xr_orig)
+Xu = copy(Xu_orig)
+Xv = copy(Xv_orig)
+Xw = copy(Xw_orig)
+Xbx = copy(Xbx_orig)
+Xby = copy(Xby_orig)
+Xbz = copy(Xbz_orig)
+
+## Center the data
+Xrbar = mean(Xr, dims=2)
+Xubar = mean(Xu, dims=2)
+Xvbar = mean(Xv, dims=2)
+Xwbar = mean(Xw, dims=2)
+Xbxbar = mean(Xbx, dims=2)
+Xbybar = mean(Xby, dims=2)
+Xbzbar = mean(Xbz, dims=2)
+
+if CENTER_DATA
+    @info "Centering the data"
+    Xr .-= Xrbar
+    Xu .-= Xubar
+    Xv .-= Xvbar
+    Xw .-= Xwbar
+    Xbx .-= Xbxbar
+    Xby .-= Xbybar
+    Xbz .-= Xbzbar
+end
+
+## Save mean data
+if !isfile(joinpath(FILEPATH, "data/mean.jld2"))
+    @info "Saving mean data to file"
+    save(joinpath(FILEPATH, "data/mean.jld2"),
+        "mean", Dict(
+            "rho" => Xrbar, "u" => Xubar, "v" => Xvbar, "w" => Xwbar,
+            "Bx" => Xbxbar, "By" => Xbybar, "Bz" => Xbzbar
+        )
+    )
+end
+
+## Normalize to [0,1] with (row-wise) minmax scaling
+SCALE_TYPE = :minmax
+function minmax_shift_scale(X)
+    X_min = minimum(X, dims=2)
+    X_max = maximum(X, dims=2)
+    X .-= X_min 
+    X ./= (X_max - X_min)
+    return X, X_min, X_max
+end
+
+function minmaxsym_shift_scale(X)
+    X_min = minimum(X, dims=2)
+    X_max = maximum(X, dims=2)
+    X .-= (0.5 * (X_max + X_min))
+    X ./= (0.5 * (X_max - X_min))
+    return X, X_min, X_max
+end
+
+if SCALE_DATA
+    if SCALE_TYPE == :minmax
+        @info "Scaling the data to [0, 1] with minmax scaling"
+        Xr, Xr_min, Xr_max = minmax_shift_scale(Xr)
+        Xu, Xu_min, Xu_max = minmax_shift_scale(Xu)
+        Xv, Xv_min, Xv_max = minmax_shift_scale(Xv)
+        Xw, Xw_min, Xw_max = minmax_shift_scale(Xw)
+        Xbx, Xbx_min, Xbx_max = minmax_shift_scale(Xbx)
+        Xby, Xby_min, Xby_max = minmax_shift_scale(Xby)
+        Xbz, Xbz_min, Xbz_max = minmax_shift_scale(Xbz)
+    elseif SCALE_TYPE == :minmaxsym
+        @info "Scaling the data to [-1, 1] with minmax scaling"
+        Xr, Xr_min, Xr_max = minmaxsym_shift_scale(Xr)
+        Xu, Xu_min, Xu_max = minmaxsym_shift_scale(Xu)
+        Xv, Xv_min, Xv_max = minmaxsym_shift_scale(Xv)
+        Xw, Xw_min, Xw_max = minmaxsym_shift_scale(Xw)
+        Xbx, Xbx_min, Xbx_max = minmaxsym_shift_scale(Xbx)
+        Xby, Xby_min, Xby_max = minmaxsym_shift_scale(Xby)
+        Xbz, Xbz_min, Xbz_max = minmaxsym_shift_scale(Xbz)
+    else
+        error("Unknown scaling type: $SCALE_TYPE")
+    end
+end
+
+## Save preprocessed data
+preprocessed_file = joinpath(FILEPATH, "data/preprocessed_data.jld2")
+if !isfile(preprocessed_file)
+    @info "Saving preprocessed data to file"
+    save(preprocessed_file, 
+        "X", Dict(
+            "rho" => Xr, "u" => Xu, "v" => Xv, "w" => Xw,
+            "Bx" => Xbx, "By" => Xby, "Bz" => Xbz,
+            "all" => vcat(Xr, Xu, Xv, Xw, Xbx, Xby, Xbz)
+        )
+    )
+end
+
+## Save shift and scaling 
+if SCALE_DATA
+    minmax_file = joinpath(FILEPATH, "data/minmax.jld2")
+    if !isfile(minmax_file)
+        if SCALE_TYPE == :minmax
+            @info "Saving min/max [0, 1] scaling parameters to file"
+            save(minmax_file, 
+                "shift", Dict(
+                "rho" => Xr_min, "u" => Xu_min, "v" => Xv_min, "w" => Xw_min,
+                "Bx" => Xbx_min, "By" => Xby_min, "Bz" => Xbz_min
+                ),
+                "scale", Dict(
+                "rho" => Xr_max - Xr_min, "u" => Xu_max - Xu_min, 
+                "v" => Xv_max - Xv_min, "w" => Xw_max - Xw_min,
+                "Bx" => Xbx_max - Xbx_min, "By" => Xby_max - Xby_min,
+                "Bz" => Xbz_max - Xbz_min
+                ),
+            )
+        elseif SCALE_TYPE == :minmaxsym
+            @info "Saving min/max [-1, 1] scaling parameters to file"
+            save(minmax_file, 
+                "shift", Dict(
+                "rho" => 0.5 * (Xr_max + Xr_min), 
+                "u" => 0.5 * (Xu_max + Xu_min), 
+                "v" => 0.5 * (Xv_max + Xv_min), 
+                "w" => 0.5 * (Xw_max + Xw_min),
+                "Bx" => 0.5 * (Xbx_max + Xbx_min),
+                "By" => 0.5 * (Xby_max + Xby_min),
+                "Bz" => 0.5 * (Xbz_max + Xbz_min)
+                ),
+                "scale", Dict(
+                "rho" => 0.5 * (Xr_max - Xr_min), 
+                "u" => 0.5 * (Xu_max - Xu_min), 
+                "v" => 0.5 * (Xv_max - Xv_min), 
+                "w" => 0.5 * (Xw_max - Xw_min),
+                "Bx" => 0.5 * (Xbx_max - Xbx_min),
+                "By" => 0.5 * (Xby_max - Xby_min),
+                "Bz" => 0.5 * (Xbz_max - Xbz_min)
+                ),
+            )
+        else
+            error("Unknown scaling type: $SCALE_TYPE")
+        end
+    end
+end
---
+++ scripts/Two-Pass_Streaming-OpInf/mhd64/01-energy.jl
@@ -0,0 +1,151 @@
+"""
+Supernova 64^3 example: Computing the energy spectrum
+"""
+
+#=================#
+## Load packages ##
+#=================#
+using LinearAlgebra
+using FileIO
+using JLD2
+using Statistics
+
+#=============#
+## Load data ##
+#=============#
+# Get paths and data file name
+FILEPATH = occursin("scripts", pwd()) ? 
+           joinpath(pwd(), "Two-Pass_Streaming-OpInf/mhd64") : 
+           joinpath(pwd(), "scripts/Two-Pass_Streaming-OpInf/mhd64")
+DATAPATH = "../../../../DATA/THE_WELL/mhd64"
+train_files = readdir(DATAPATH, join=true)
+fn = train_files[1]
+
+# Include the data sourcing module for data access
+include(joinpath(FILEPATH, "datasource.jl"))
+
+# Load data source 
+ds = DataSource(fn)
+nx, ny, nz, n_fields, n_time, n_traj = ds.dims
+nxyz = nx * ny * nz
+n = n_time * n_traj
+
+#=============#
+## Load data ##
+#=============#
+preprocessed_file = joinpath(FILEPATH, "data/preprocessed_data.jld2")
+X = load(preprocessed_file, "X")
+
+#===========================#
+## Compute singular values ##
+#===========================#
+singular_values = Dict(
+    fld => svdvals(X[fld]) for fld in ds.fields
+)
+
+#===============================#
+## Check the energy retainment ##
+#===============================#
+function check_energy_retainment(svals, target=0.999)
+    energy = sum(svals.^2)
+    energy_ret = cumsum(svals.^2) / energy
+    r = findfirst(x -> x > target, energy_ret) + 1
+    return energy_ret, r
+end
+
+spectrum = Dict(fn => zeros(length(singular_values[fn])) for fn in ds.fields)
+target_r = Dict(fn => 0 for fn in ds.fields)
+
+target_energy = 0.90
+for fld in ds.fields
+    svals = singular_values[fld]
+    spectrum[fld], target_r[fld] = check_energy_retainment(svals, target_energy)
+end
+
+# Save target ranks
+target_r_file = joinpath(FILEPATH, "data/target_ranks.jld2")
+save(target_r_file, "target_ranks", target_r)
+
+
+#============================#
+## Plot the energy spectrum ##
+#============================#
+using CairoMakie
+
+with_theme(theme_latexfonts()) do 
+    fig = Figure(size=(1400, 1000))
+    ax1 = Axis(
+        fig[2, 1], # xlabel=L"singular value index, $i$", 
+        ylabel="energy retainment",
+        limits=(-50, length(spectrum["rho"])+10, nothing, nothing),
+        # xgridvisible=false, ygridvisible=false,
+        topspinevisible=false, rightspinevisible=false,
+        titlesize=30, xlabelsize=30, ylabelsize=30, 
+        xticklabelsize=25, yticklabelsize=25,
+        yticks=(0:0.1:1, ["0%", "10%", "20%", "30%", "40%", 
+                          "50%", "60%", "70%", "80%", "90%", "100%"])
+    )
+
+    colors = Dict(
+        key => color for (key, color) in zip(
+            ds.fields, Makie.wong_colors()[1:length(ds.fields)]
+        )
+    )
+
+    # Plot energy retainment for each field
+    for fld in ds.fields
+        svals = singular_values[fld]
+        lines!(ax1, 1:length(svals), spectrum[fld], 
+               linewidth=4, color=colors[fld])
+    end
+
+    # Add horizontal line at 99.9% energy retention
+    hlines!(ax1, [target_energy], color=:black, linestyle=:dash, linewidth=4)
+
+    ax2 = Axis(
+        fig[3, 1], xlabel="singular value index", 
+        # xgridvisible=false, ygridvisible=false,
+        ylabel="singular value", yscale=log10,
+        topspinevisible=false, rightspinevisible=false, ylabelpadding=30,
+        limits=(-50, length(spectrum["rho"])+10, nothing, nothing),
+        titlesize=30, xlabelsize=30, ylabelsize=30, 
+        xticklabelsize=25, yticklabelsize=25,
+    )
+
+    for fld in ds.fields
+        svals = singular_values[fld]
+        lines!(ax2, 1:length(svals), svals, label=fld, linewidth=4, 
+               color=colors[fld])
+    end
+
+    # Add vertical lines for target ranks
+    for (key, r) in target_r
+        if r > 0
+            vlines!(ax1, [r], linestyle=:dash, color=colors[key],
+                    linewidth=4, label="target rank for $key")
+            vlines!(ax2, [r], linestyle=:dash, color=colors[key],
+                    linewidth=4, label="target rank for $key")
+        end
+    end
+
+    # Add legend
+    # Legend(fig[:, 2], ax1, framevisible=false, labelsize=25)
+    line_elements = [
+        [LineElement(color=colors[fn], linewidth=5)]
+        for fn in ds.fields
+    ]
+    labels = [
+        fld == "rho" ? L"$\rho$" : 
+        occursin("B", fld) ? L"$B_{%$(fld[2])}$" :
+        L"$%$(fld)$" for fld in ds.fields
+    ]
+    Legend(fig[1, :],
+        line_elements, labels,
+        framevisible=false, patchsize=(70, 20),
+        labelsize=40, rowgap=10, colgap=50, orientation=:horizontal,
+    )
+    
+    # Display figure
+    display(fig)
+    save(joinpath(FILEPATH, "plots/energy_spectrum.png"), fig, px_per_inch=200)
+end
