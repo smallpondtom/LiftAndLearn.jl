ee491b031a787da784ef89b0ebbb03380cc59973
smallpondtom <tkoike45@gmail.com>
2025-08-21 08:06:48 -0400

    remove limits from a plot

--- scripts/Two-Pass_Streaming-OpInf/heat2d/05-plot.jl
+++ scripts/Two-Pass_Streaming-OpInf/heat2d/05-plot.jl
@@ -360,6 +360,7 @@ with_theme(theme_latexfonts()) do
         yscale=log10, 
         xticks=xtick_vals, titlesize=30, 
         xlabelsize=30, ylabelsize=30, xticklabelsize=25, yticklabelsize=25,
+        limits=(nothing, nothing, 1e-9, 3e0),
     )
     for (j,ri) in enumerate(1:rmax)  # over all reduced dimensions
         scatterlines!(ax1, 1:num_of_streams, stream_res[:rls].stream_err[ri,:], color=line_colors[j])
@@ -371,6 +372,7 @@ with_theme(theme_latexfonts()) do
         yscale=log10, 
         xticks=xtick_vals, titlesize=30, 
         xlabelsize=30, ylabelsize=30, xticklabelsize=25, yticklabelsize=25,
+        limits=(nothing, nothing, 1e-9, 3e0),
     )
     for (j,ri) in enumerate(1:rmax)  # over all reduced dimensions
         scatterlines!(ax2, 1:num_of_streams, stream_res[:iqrrls].stream_err[ri,:], color=line_colors[j])
@@ -382,6 +384,7 @@ with_theme(theme_latexfonts()) do
         yscale=log10, 
         xticks=xtick_vals, titlesize=30, 
         xlabelsize=30, ylabelsize=30, xticklabelsize=25, yticklabelsize=25,
+        limits=(nothing, nothing, 1e-9, 3e0),
     )
     lines = []
     labels = []
@@ -392,7 +395,7 @@ with_theme(theme_latexfonts()) do
     end
     Legend(fig[1,4], lines, labels, labelsize=30)
     display(fig)
-    save(joinpath(FILEPATH, "plots/rel_stream_err_per_stream.pdf"), fig)
+    # save(joinpath(FILEPATH, "plots/rel_stream_err_per_stream.pdf"), fig)
 end
 
 #=====================================================#
