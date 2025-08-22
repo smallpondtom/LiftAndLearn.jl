ba0becdf004e1561a1aa419e190df4484113793a
smallpondtom <tkoike45@gmail.com>
2025-08-22 08:21:33 -0400

    minor change

--- scripts/Two-Pass_Streaming-OpInf/burgers/01-datagen.jl
+++ scripts/Two-Pass_Streaming-OpInf/burgers/01-datagen.jl
@@ -18,7 +18,9 @@ import LiftAndLearn as LnL
 #================================#
 ## Configure filepath for saving
 #================================#
-FILEPATH = occursin("scripts", pwd()) ? joinpath(pwd(),"Two-Pass_Streaming-OpInf/burgers") : joinpath(pwd(), "scripts/Two-Pass_Streaming-OpInf/burgers")
+FILEPATH = occursin("scripts", pwd()) ? 
+           joinpath(pwd(),"Two-Pass_Streaming-OpInf/burgers") : 
+           joinpath(pwd(), "scripts/Two-Pass_Streaming-OpInf/burgers")
 
 #=========================#
 ## Burgers equation setup
