b7887e5b4418337fc8e50e267d3fb08622a841e1
smallpondtom <tkoike45@gmail.com>
2025-09-01 15:52:43 -0400

    update one-pass abl

--- scripts/One-Pass_Streaming-OpInf/ABL/02-train-stream.jl
+++ scripts/One-Pass_Streaming-OpInf/ABL/02-train-stream.jl
@@ -40,7 +40,6 @@ include(joinpath(FILEPATH, "preprocess.jl"))
 ds = ChannelDataSource(datafile, ["z", "y", "x", "fields", "times"])
 Nz, Ny, Nx, n_fields, n = ds.dims
 nxyz = Nz * Ny * Nx
-dPdx = 0.001722
 n_test = 2000
 n_train = n - n_test
 
@@ -78,22 +77,48 @@ rmax = 400
 options.with_reg = true
 options.λ = LnL.TikhonovParameter(A=1e12, K=1e12, A2=1e12)
 stream = LnL.OnePassStreamingOpInf(
-    ds[1]; 
+    preprocess!(ds[1], means, shifts, scales); 
     options=options, 
     n=Int(nxyz * n_fields), 
     rank=rmax, 
     finite_diff=true
 )
 @showprogress for i in 2:n_train
-    LnL.stream!(stream, ds[i], tol=1e-10)
+    LnL.stream!(stream, preprocess!(ds[i], means, shifts, scales), tol=1e-10)
 end
 E, Δidx = LnL.finite_diff_matrix(
     options.data.deriv_type, n_train, options.data.Δt
 )
-op_stream = LnL.compute_stream_operators(
+
+# r = 400
+op_stream_r400 = LnL.compute_stream_operators(
     stream, E, (Δidx[1], Δidx[end])
 )
 
+# r = 350
+op_stream_r350 = LnL.compute_stream_operators(
+    stream, E, (Δidx[1], Δidx[end]); rank=350
+)
+
+# r = 300
+op_stream_r300 = LnL.compute_stream_operators(
+    stream, E, (Δidx[1], Δidx[end]); rank=300
+)
+
+# r = 250
+op_stream_r250 = LnL.compute_stream_operators(
+    stream, E, (Δidx[1], Δidx[end]); rank=250
+)
+
+# r = 200
+op_stream_r200 = LnL.compute_stream_operators(
+    stream, E, (Δidx[1], Δidx[end]); rank=200
+)
+
 # Save the stream object and operators
-save(joinpath(FILEPATH, "data/results/onepass_stream.jld2"), 
-     "stream", stream, "op_stream", op_stream)
+save(joinpath(FILEPATH, "data/results/onepass_stream.jld2"), "stream", stream)
+save(joinpath(FILEPATH, "data/results/op_stream_r400.jld2"), "op_stream_r400", op_stream_r400)
+save(joinpath(FILEPATH, "data/results/op_stream_r350.jld2"), "op_stream_r350", op_stream_r350)
+save(joinpath(FILEPATH, "data/results/op_stream_r300.jld2"), "op_stream_r300", op_stream_r300)
+save(joinpath(FILEPATH, "data/results/op_stream_r250.jld2"), "op_stream_r250", op_stream_r250)
+save(joinpath(FILEPATH, "data/results/op_stream_r200.jld2"), "op_stream_r200", op_stream_r200)
